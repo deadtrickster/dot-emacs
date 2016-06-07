2c1c5737f861c626163b45ae966cc36f4d93a73d
Ilya Khaprov <dead.trickster@gmail.com>
2016-06-07 19:25:32 +0300

    bring selected-packages list back

--- init.el
+++ init.el
@@ -5,10 +5,6 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(load "~/.emacs.d/paths.el")
-
-(require 'settings)
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -16,6 +12,8 @@
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-echo-area-message "dead")
  '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote (iedit alchemist web-mode rainbow-mode erlang ac-slime js2-refactor js2-mode paredit paren-face auto-complete go-autocomplete go-eldoc yasnippet flycheck go-mode highlight-numbers hl-todo)))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -25,6 +23,10 @@
  '(default ((t (:family "Roboto Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal))))
  '(parenthesis ((t (:inherit default :foreground "dim gray"))))
  '(show-paren-match ((t (:background "#2f334b")))))
+
+(load "~/.emacs.d/paths.el")
+
+(require 'settings)
 
 (server-start)
 
--- settings/general/settings-packages.el
+++ settings/general/settings-packages.el
@@ -3,9 +3,6 @@
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 
-(setf package-selected-packages
-      '(iedit alchemist web-mode rainbow-mode erlang ac-slime js2-refactor js2-mode paredit paren-face auto-complete go-autocomplete go-eldoc yasnippet flycheck go-mode highlight-numbers hl-todo))
-
 (let ((packages-fetched))
   (dolist (package package-selected-packages)
     (unless (package-installed-p package)
