bd2f212e8f23b2c24823d80f480065e670a468bb
Ilya Khaprov <i.khaprov@gmail.com>
2019-01-30 15:37:42 +0100

    Reuse projectile/file terminals, also hotkey

--- settings/mode/mode-vterm.el
+++ settings/mode/mode-vterm.el
@@ -30,15 +30,60 @@
                              (yas-minor-mode -1)
                              (flycheck-mode -1)))
 
-(defun vterm--rename-buffer-as-title (title)
-  (let ((dir (concat (nth 1 (split-string title ":")) "/")))
-    (cd dir))
-  (rename-buffer (format "term %s" title)))
-
-(add-hook 'vterm-set-title-functions 'vterm--rename-buffer-as-title)
+;; (defun vterm--rename-buffer-as-title (title)
+;;   (let ((dir (concat (nth 1 (split-string title ":")) "/")))
+;;     (cd dir))
+;;   (rename-buffer (format "term %s" title)))
+;;
 
 (define-key vterm-mode-map [?\C-c] nil)
 (define-key vterm-mode-map [?\C-c ?\C-c] 'vterm--self-insert)
 (define-key vterm-mode-map [delete] #'vterm--self-insert)
+
+(make-local-variable 'vterm-is-for-project)
+(setq-default vterm-is-for-project nil)
+
+(defun vterm--rename-buffer-as-title (title)
+  (let ((dir (concat (nth 1 (split-string title ":")) "/")))
+    (cd dir)
+    (rename-buffer (if vterm-is-for-project
+                       (vterm--project-buffer-title)
+                     (vterm--buffer-title)))))
+
+(add-hook 'vterm-set-title-functions 'vterm--rename-buffer-as-title)
+
+(defun vterm--buffer-title ()
+  (format "term %s@%s:%s" user-login-name (system-name) default-directory))
+
+(defun vterm--project-buffer-title ()
+  (format "term %s" (projectile-project-name)))
+
+(defun vt ()
+  "Create a new vterm."
+  (interactive)
+  (let* ((buffer-title (vterm--buffer-title))
+         (existing-buffer (get-buffer buffer-title)))
+    (if existing-buffer
+        (switch-to-buffer existing-buffer)
+      (let ((buffer (generate-new-buffer buffer-title)))
+        (with-current-buffer buffer
+          (vterm-mode))
+        (switch-to-buffer buffer)))))
+
+(defun vtp ()
+  "Create a new vterm."
+  (interactive)
+  (let* ((buffer-title (vterm--project-buffer-title))
+         (existing-buffer (get-buffer buffer-title)))
+    (if existing-buffer
+        (switch-to-buffer existing-buffer)
+      (let ((buffer (generate-new-buffer buffer-title)))
+        (with-current-buffer buffer
+          (cd (projectile-project-root))
+          (vterm-mode)
+          (setq-local vterm-is-for-project t))
+        (switch-to-buffer buffer)))))
+
+(define-key global-map [?\C-t] 'vtp)
 
 (provide 'mode-vterm)
