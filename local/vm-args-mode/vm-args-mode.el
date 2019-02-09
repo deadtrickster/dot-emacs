0c39733aa4126c0b87e3bdc073e4490fb3b665ca
Ilya Khaprov <i.khaprov@gmail.com>
2019-02-09 19:47:23 +0100

    generalize vm.args mode a bit

--- local/vm-args-mode/vm-args-mode.el
+++ local/vm-args-mode/vm-args-mode.el
@@ -10,7 +10,6 @@
     table)
   "Syntax table for `vm-args-mode'.")
 
-
 (defface vm-args-font-lock-flags-face
   '((default (:inherit font-lock-keyword-face)))
   "Face used for highlighting flags."
@@ -20,40 +19,37 @@
   'vm-args-font-lock-flags-face)
 
 (defvar vm-args-font-lock
-  `(,(cons (eval-when-compile
-	     (regexp-opt '("-mode" "-hidden") t))
+  `(,(cons "\\(\\(-\\|\\+\\)\\w*\\)"
            vm-args-font-lock-flags-face))
   "Default font-lock-keywords for `vm-args mode'.")
 
 (setf vm-args-font-lock
-  `(,(cons (eval-when-compile
-	     (regexp-opt '("-name"
-			   "-mode"
-			   "-hidden"
-			   "-setcookie"
-			   "-pa") t))
-           vm-args-font-lock-flags-face)))
+  `(,(cons "\\(\\(-\\|\\+\\)\\(\\w\\|_\\)*\\)"
+           vm-args-font-lock-flags-face)
+
+    ,(cons (eval-when-compile
+             (regexp-opt '("true"
+                           "false")
+                         'words))
+           font-lock-constant-face)))
 
 (define-abbrev-table 'vm-args-mode-abbrev-table nil
   "Abbrev table used while in `vm-args-mode'.")
-
-(unless vm-args-mode-abbrev-table
-  (define-abbrev-table 'vm-args-mode-abbrev-table ()))
 
 ;;;###autoload
 (define-derived-mode vm-args-mode vm-args-parent-mode "vm.args"
   "A major mode to edit BEAM vm.args file.
 \\{vm-args-mode-map}
 "
+  (setq local-abbrev-table (or vm-args-mode-abbrev-table
+                               (define-abbrev-table 'vm-args-mode-abbrev-table ())))
   (set-syntax-table vm-args-mode-syntax-table)
   (set (make-local-variable 'require-final-newline) mode-require-final-newline)
   (set (make-local-variable 'comment-start) "#")
   (set (make-local-variable 'comment-end) "")
   (set (make-local-variable 'comment-start-skip) "#+ *")
   (set (make-local-variable 'parse-sexp-ignore-comments) t)
-  (set (make-local-variable 'font-lock-defaults)
-       '(vm-args-font-lock nil t))
-  (setq local-abbrev-table vm-args-mode-abbrev-table))
+  (set (make-local-variable 'font-lock-defaults) '(vm-args-font-lock nil t)))
 
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("vm.args" . vm-args-mode))
